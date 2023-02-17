984b1451bf371d6e6aaa69b5ef28080ae7880993
marvintalavera <talavera.marvin@rocketmail.com>
2023-02-16 21:41:06 -0800

    Delete functionality implemented

--- controllers/places.js
+++ controllers/places.js
@@ -29,7 +29,7 @@ router.get('/:id', (req, res) => {
     res.render('error404')
   }
   else {
-    res.render('places/show', { place: places[id] })
+    res.render('places/show', { place: places[id], id })
   }
 })
 router.post('/', (req, res) => {
@@ -45,6 +45,20 @@ router.post('/', (req, res) => {
   }
   places.push(req.body)
   res.redirect('/places')
+})
+
+router.delete('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) {
+    res.render('error404')
+  }
+  else {
+    places.splice(id, 1)
+    res.redirect('/places')
+  }
 })
 
 module.exports = router
