462800f418bc9b1ff08e959d962d505d1ddbe6c2
marvintalavera <talavera.marvin@rocketmail.com>
2023-02-16 17:54:14 -0800

    Adding Bootstrap grid to index page

--- views/error404.jsx
+++ views/error404.jsx
@@ -4,10 +4,16 @@ const Def = require('./default')
 function error404 () {
     return (
       <Def>
-          <main>
-              <h1>404: PAGE NOT FOUND</h1>
-              <p>Oops, sorry, we can't find this page!</p>
-          </main>
+      <main>
+        <h1>404: PAGE NOT FOUND</h1>
+        <p>Oops, sorry we can't find this page.</p>
+        <div>
+          <img src="/images/cow.jpg" alt="cow licking" />
+          <div>
+          Photo by <a href="https://unsplash.com/@ainarscekuls?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Ainārs Cekuls</a> on <a href="https://unsplash.com/photos/h-XRaPA0b9o?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+          </div>
+        </div>
+      </main>
       </Def>
     )
   }
--- views/home.jsx
+++ views/home.jsx
@@ -2,17 +2,23 @@ const React = require('react')
 const Def = require('./default')
 
 function home () {
-    return (
-      <Def>
-          <main>
-              <h1>HOME</h1>
-              <a href="/places">
-              <button className="btn-primary">Places Page</button>
-              </a>
-          </main>
-      </Def>
+  return (
+    <Def>
+      <main>
+        <h1>HOME</h1>
+        <div>
+          <img src="/images/bird.jpg" alt="bird chilling" />
+          <div>
+          Photo by <a href="https://unsplash.com/@brewbottle?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Bob Brewer</a> on <a href="https://unsplash.com/photos/HsXPfq5lKHc?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+          </div>
+        </div>
+        <a href="/places">
+          <button className="btn-primary">Places Page</button>
+        </a>
+</main>
+
+        </Def>
     )
-  }
-  
+}
 
 module.exports = home
