745ba2389d7331f1d828cd23b4b56f28dc287af0
marvintalavera <talavera.marvin@rocketmail.com>
2023-02-25 23:40:50 -0800

    comments console log but don't add to database

--- controllers/places.js
+++ controllers/places.js
@@ -27,15 +27,17 @@ router.get("/new", (req, res) => {
   res.render("places/new");
 });
 //  show
-router.get("/:id", (req, res) => {
+router.get('/:id', (req, res) => {
   db.Place.findById(req.params.id)
-    .then((place) => {
-      res.render("places/show", { place });
-    })
-    .catch((err) => {
-      console.log("err", err);
-      res.render("error404");
-    });
+  .populate('comments')
+  .then(place => {
+      console.log(place.comments)
+      res.render('places/show', { place })
+  })
+  .catch(err => {
+      console.log('err', err)
+      res.render('error404')
+  });
 });
 
 // delete
@@ -56,4 +58,27 @@ router.put("/:id", (req, res) => {
 router.get("/:id/edit", (req, res) => {
   res.send("GET edit form stub");
 });
+
+router.post('/:id/comment', (req, res) => {
+  console.log(req.body)
+  db.Place.findById(req.params.id)
+  .then(place => {
+      db.Comment.create(req.body)
+      .then(comment => {
+          place.comments.push(comment.id)
+          place.save()
+          .then(() => {
+              res.redirect(`/places/${req.params.id}`)
+          })
+      })
+      .catch(err => {
+          res.render('error404')
+      })
+  })
+  .catch(err => {
+      res.render('error404')
+  })
+})
+
+
 module.exports = router;
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -1,9 +1,29 @@
 const React = require("react");
 const Def = require("../default");
 function show(data) {
+  let comments = (
+    <h3 className="inactive">
+      No comments yet!
+    </h3>
+  )
+  if (data.place.comments.length) {
+    comments = data.place.comments.map(c => {
+      return (
+        <div className="border">
+          <h2 className="rant">{c.rant ? 'Rant! ðŸ˜¡' : 'Rave! ðŸ˜»'}</h2>
+          <h4>{c.content}</h4>
+          <h3>
+            <stong>- {c.author}</stong>
+          </h3>
+          <h4>Rating: {c.stars}</h4>
+        </div>
+      )
+    })
+  }
   return (
     <Def>
-      <main>        <div className="row">
+      <main>        
+        <div className="row">
           <div className="col-sm-6">
             <h1>{data.place.name}</h1>
             <img src={data.place.pic} alt={data.place.name} />
@@ -18,8 +38,29 @@ function show(data) {
             <h2>Description</h2>
             <h3>{data.place.showEstablished()}</h3>
             <h4>Serving {data.place.cuisines}</h4>
-            <h2>Comments</h2>
-            <p>No comments yet!</p>
+            <form method="POST" action="/places/{place.id}/comment" className="comment-form">
+            <legend className='comment-legend'><h4>Add A Comment</h4></legend>
+            <div className="comment-auth">
+            <label htmlFor="Author">Author:</label>
+            <input type="text" name="author"/>
+            </div>
+            <div className="comment-stars">
+            <label htmlFor="stars">Rating:</label>
+            <input type="number" name="stars" step="0.5" min="0.5" max="5" defaultValue="0.5" />
+            </div>
+            <div className="comment-rant">
+            <label htmlFor="rant">Rant:</label>
+            <input type="checkbox" name="rant" value="rant" />
+            </div>
+            <div className="comment-content">
+            <label htmlFor="content">Content</label>
+            <textarea name="content" rows="5"></textarea>
+            </div>
+            <div className="comment-submit">
+            <button type="submit">Comment</button>
+            </div>
+            </form>
+            {comments}
             <a href={`/places/${data.id}/edit`} className="btn btn-warning">
               Edit
             </a>
