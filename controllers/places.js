e2f7cfb7fb3cf3435223090f659e600d38ec18d8
marvintalavera <talavera.marvin@rocketmail.com>
2023-02-25 21:40:37 -0800

    assignment 9

--- controllers/places.js
+++ controllers/places.js
@@ -1,45 +1,59 @@
 const router = require("express").Router();
 const db = require("../models");
-
-
+// index
 router.get("/", (req, res) => {
   db.Place.find()
-  .then((places) => {
-    res.render("places/index", { places });
-  })
-  .catch((err) => {
-    console.log(err);
-    res.render(404);
-  });
+    .then((places) => {
+      res.render("places/index", { places });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.render(404);
+    });
 });
-
+// create
 router.post("/", (req, res) => {
   db.Place.create(req.body)
-  .then(() => {
-    res.redirect("/places");
-  })
-  .catch((err) => {
-    console.log("err", err);
-    res.render("error404");
-  });
+    .then(() => {
+      res.redirect("/places");
+    })
+    .catch((err) => {
+      console.log("err", err);
+      res.render("error404");
+    });
 });
-
+//  new
 router.get("/new", (req, res) => {
   res.render("places/new");
 });
-
+//  show
 router.get("/:id", (req, res) => {
-  res.send("GET /places/:id stub");
+  db.Place.findById(req.params.id)
+    .then((place) => {
+      res.render("places/show", { place });
+    })
+    .catch((err) => {
+      console.log("err", err);
+      res.render("error404");
+    });
 });
+
+// delete
+router.delete("/:id", (req, res) => {
+  db.Place.deleteOne({ id: req.params.id })
+    .then(() => res.redirect("/places"))
+    .catch((err) => {
+      console.log(err);
+      res.status(404).render("error404");
+    });
+});
+
+//  put
 router.put("/:id", (req, res) => {
   res.send("PUT /places/:id stub");
 });
-
-router.delete("/:id", (req, res) => {
-  res.send("DELETE /places/:id stub");
-});
+// edit
 router.get("/:id/edit", (req, res) => {
   res.send("GET edit form stub");
 });
-
 module.exports = router;
--- models/index.js
+++ models/index.js
@@ -1,8 +1,11 @@
-const mongoose = require("mongoose");
+require('dotenv').config()
+const mongoose = require('mongoose')
+
+mongoose.set('strictQuery', true);
 
 mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+  useNewUrlParser: true, 
+  useUnifiedTopology: true
+})
 
-module.exports.Place = require("./places");
+module.exports.Place = require('./places')
